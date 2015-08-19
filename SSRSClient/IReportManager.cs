1b0273140d0fdb14caf3ac7a18842c9d8ac9ac1c
varunrl <varun.robinson@gmail.com>
2015-08-19 12:54:51 +0530

    SSRS2008 Added

---
+++ SSRSClient/IReportManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSRSClient
+{
+    public interface IReportManager
+    {
+        bool createDatasource(string localFilePath, string serverFolderPath, bool overWrite);
+        string DeployReport(string localPath, string serverPath);
+        void DownloadReports(string localDirectory, string CurrentReportFolderPath);
+        List<CatalogObject> GetDataSources(string serverPath);
+
+        List<CatalogObject> GetFolders(string serverPath);
+        List<DataSourceObject> GetFoldersWithDetails(string serverPath);
+
+        void SetDataSources(List<DataSourceObject> list, string datasourcePath);
+    }
+}
---
+++ SSRSClient/Utility.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSRSClient
+{
+    public static class  Utility
+    {
+
+        public static string GetFileType(string p)
+            {
+                switch (System.IO.Path.GetExtension(p))
+                {
+                    case ".rdl":
+                        return "Report";
+
+                    case ".rds":
+                        return "DataSource";
+
+                    case ".rsd":
+                        return "DataSet";
+
+                    default:
+                        return "NA";
+                }
+            }
+    }
+}
